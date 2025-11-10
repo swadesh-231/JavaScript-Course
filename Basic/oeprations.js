08cc9a5cd669992f153ed5a2f4244e8e04b29212
swadesh-231 <swadeshchatterjee720@gmail.com>
2025-11-10 22:18:28 +0530

    commit two

---
+++ Basic/oeprations.js
@@ -0,0 +1,29 @@
+
+let number = 23;
+
+let negative_value = -number
+console.log(negative_value);
+
+let str  = "Hello"
+let btr = "World"
+
+console.log(str+" "+ btr);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
